976af02a3edfbca8fe4a84855e781e0aa0c84969
Aida Abdikulova <abdikulova@gmail.com>
2019-08-01 02:30:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Song
-  
+
   extend Memorable
 
   attr_accessor :name
@@ -20,14 +20,6 @@ class Song
   def self.all
     @@songs
   end
-
-  #def self.reset_all
-  #  self.all.clear
-  #end
-
-  #def self.count
-  #  self.all.count
-  #end
 
   def artist=(artist)
     @artist = artist
